8143e86445d23c80e755761fb68a4bcae65cc37e
Amir <60928821+Amir-A-M@users.noreply.github.com>
2022-05-21 15:19:06 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const htmlTag = document.documentElement;
+  const [mode, setMode] = useState("light");
   return (
-    <div className="App">
-      
+    <div className="App flex justify-center items-center h-screen bg-white dark:bg-black">
+      <button className="text-4xl" onClick={toggleMode}>{mode}</button>
     </div>
   );
+
+  function toggleMode() {
+    const newMode = mode === "light" ? "dark" : "light";
+
+    if (mode === "dark") htmlTag.classList.add("dark");
+    else htmlTag.classList.remove("dark");
+    setMode(newMode);
+  }
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  important: "html",
+
+  // This configuration will prefix all of your utilities with the given selector,
+  // effectively increasing their specificity without actually making them !important.
+  // important: "#important",
+
+  // If you want to support toggling dark mode manually instead of relying on the operating system preference,
+  // use the class strategy instead of the media strategy:
   darkMode: "class",
+  
   theme: {
     extend: {},
   },
